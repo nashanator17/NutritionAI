ddc19cd83a3bde445cc69267286a5153d287028c
danieljabbour <jabbourd@mcmaster.ca>
2018-06-19 14:22:43 -0400

    added routes for images and keys

--- clarifai_vision.py
+++ clarifai_vision.py
@@ -1,30 +1,22 @@
-#
-# Pip install the client:
-# pip install clarifai
-#
-
-# The package will be accessible by importing clarifai:
-
 import io
 import json
 
+#python3 -m pip install clarifai
 from clarifai import rest
 from clarifai.rest import ClarifaiApp
 from clarifai.rest import Image as CImage
 
-# The client takes the `API_KEY` you created in your Clarifai
-# account. You can set these variables in your environment as:
+def post(img_path):
+    f = open("key_clarifai.txt","r")
+    key = f.readline()
 
-f = open("key_clarifai.txt","r")
-key = f.readline()
-
-# - `CLARIFAI_API_KEY`
-app = ClarifaiApp(api_key=key)
+    # - `CLARIFAI_API_KEY`
+    app = ClarifaiApp(api_key=key)
 
 
-model = app.models.get('food-items-v1.0')
-image = CImage(url='https://samples.clarifai.com/food.jpg')
+    model = app.models.get('food-items-v1.0')
+    image = CImage(url=img_path)
 
-#output this to JSON
-with open ('./output_data/results.json', 'w') as f:
-    json.dump(model.predict([image]),f)
+    #output this to JSON
+    with open ('./output_data/results.json', 'w') as f:
+        json.dump(model.predict([image]),f)

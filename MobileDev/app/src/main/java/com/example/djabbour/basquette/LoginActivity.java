cb5770cb2d08d1c4624c1d7f9d674a3f705a5f45
danieljabbour <jabbourd@mcmaster.ca>
2018-07-11 11:57:58 -0400

    added login button navigation to main screen with tabbed view

--- MobileDev/app/src/main/java/com/example/djabbour/basquette/LoginActivity.java
+++ MobileDev/app/src/main/java/com/example/djabbour/basquette/LoginActivity.java
@@ -3,11 +3,27 @@ package com.example.djabbour.basquette;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, MainScreenActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }

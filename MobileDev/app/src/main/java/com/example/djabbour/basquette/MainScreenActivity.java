7e8aa3fe931556420a6ab958cb1bc5b82b82a64b
danieljabbour <jabbourd@mcmaster.ca>
2018-07-13 13:48:41 -0400

    Changed navigation from activities to fragments

--- MobileDev/app/src/main/java/com/example/djabbour/basquette/MainScreenActivity.java
+++ MobileDev/app/src/main/java/com/example/djabbour/basquette/MainScreenActivity.java
@@ -1,53 +1,25 @@
 package com.example.djabbour.basquette;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
-import android.widget.TextView;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class MainScreenActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        //Use switch cases to navigate to another screen
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    break;
-
-                case R.id.navigation_scan:
-                    Intent intent1 = new Intent(MainScreenActivity.this, ScanScreenActivity.class);
-                    startActivity(intent1);
-                    break;
-
-                case R.id.navigation_settings:
-                    Intent intent2 = new Intent(MainScreenActivity.this, ScanScreenActivity.class);
-                    startActivity(intent2);
-                    break;
-
-            }
-
-            return false;
-        }
-    };
+public class MainScreenActivity extends Fragment {
+    public static MainScreenActivity newInstance() {
+        MainScreenActivity fragment = new MainScreenActivity();
+        return fragment;
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_screen);
-        System.loadLibrary("opencv_java3");
+    }
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_main_screen, container, false);
     }
 }
-
--- MobileDev/app/src/main/java/com/example/djabbour/basquette/ScanScreenActivity.java
+++ MobileDev/app/src/main/java/com/example/djabbour/basquette/ScanScreenActivity.java
@@ -1,15 +1,25 @@
 package com.example.djabbour.basquette;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-    public class ScanScreenActivity extends AppCompatActivity {
-
-        @Override
-        protected void onCreate(@Nullable Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_scan_screen);
-        }
-
+public class ScanScreenActivity extends Fragment {
+    public static ScanScreenActivity newInstance() {
+        ScanScreenActivity fragment = new ScanScreenActivity();
+        return fragment;
     }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_scan_screen, container, false);
+    }
+}

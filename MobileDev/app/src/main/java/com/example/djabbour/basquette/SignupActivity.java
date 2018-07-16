375b2feddef497ad97f9fcc4dd3a7e4c68f17b9b
danieljabbour <jabbourd@mcmaster.ca>
2018-07-16 15:04:02 -0400

    Added signup screen button and navigation to main screen

--- MobileDev/app/src/main/java/com/example/djabbour/basquette/SignupActivity.java
+++ MobileDev/app/src/main/java/com/example/djabbour/basquette/SignupActivity.java
@@ -1,7 +1,10 @@
 package com.example.djabbour.basquette;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -9,6 +12,18 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        Button beginButton = (Button) findViewById(R.id.beginButton);
+
+        //Navigate to main screen on login
+        beginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignupActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 
 }

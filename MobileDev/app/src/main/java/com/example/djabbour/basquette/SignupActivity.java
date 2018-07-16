d275cf0f41c3cc7c9f39e80fe745d0c587320a40
danieljabbour <jabbourd@mcmaster.ca>
2018-07-16 14:56:01 -0400

    added signup activity route

--- MobileDev/app/src/main/java/com/example/djabbour/basquette/SignupActivity.java
+++ MobileDev/app/src/main/java/com/example/djabbour/basquette/SignupActivity.java
@@ -1,8 +1,14 @@
 package com.example.djabbour.basquette;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class SignupActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
 
 }

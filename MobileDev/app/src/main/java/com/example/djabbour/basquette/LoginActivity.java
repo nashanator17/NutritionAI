02820212834fcaf39f0f8c8c66106a237f613c76
danieljabbour <jabbourd@mcmaster.ca>
2018-07-16 13:55:22 -0400

    Synced gradle files and updated login UI

--- MobileDev/app/src/main/java/com/example/djabbour/basquette/LoginActivity.java
+++ MobileDev/app/src/main/java/com/example/djabbour/basquette/LoginActivity.java
@@ -24,7 +24,9 @@ public class LoginActivity extends AppCompatActivity {
                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                 startActivity(intent);
             }
+
         });
+
     }
 
 

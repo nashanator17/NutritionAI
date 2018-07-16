2d4566d8830308409e4512cb8ce9f554707cbd81
danieljabbour <jabbourd@mcmaster.ca>
2018-07-16 11:22:39 -0400

    Enabled openCV permissions

--- MobileDev/app/src/main/java/com/example/djabbour/basquette/ScanScreenActivity.java
+++ MobileDev/app/src/main/java/com/example/djabbour/basquette/ScanScreenActivity.java
@@ -1,5 +1,6 @@
 package com.example.djabbour.basquette;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -14,6 +15,7 @@ public class ScanScreenActivity extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
     }
 

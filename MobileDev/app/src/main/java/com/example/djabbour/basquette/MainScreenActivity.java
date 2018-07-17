b4593e6de70d061e8a6005b5828c990067e8558d
danieljabbour <jabbourd@mcmaster.ca>
2018-07-17 10:17:42 -0400

    embedded fragment in activity for use of graphs

--- MobileDev/app/src/main/java/com/example/djabbour/basquette/MainScreenActivity.java
+++ MobileDev/app/src/main/java/com/example/djabbour/basquette/MainScreenActivity.java
@@ -1,25 +1,22 @@
 package com.example.djabbour.basquette;
 
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
-public class MainScreenActivity extends Fragment {
-    public static MainScreenActivity newInstance() {
-        MainScreenActivity fragment = new MainScreenActivity();
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class MainScreenActivity extends AppCompatActivity {
+
+    public static MainScreenFragment newInstance() {
+        MainScreenFragment fragment = new MainScreenFragment();
         return fragment;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_main_screen, container, false);
-    }
 }
---
+++ MobileDev/app/src/main/java/com/example/djabbour/basquette/MainScreenFragment.java
@@ -0,0 +1,25 @@
+package com.example.djabbour.basquette;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainScreenFragment extends Fragment {
+
+    public static MainScreenFragment newInstance() {
+        MainScreenFragment fragment = new MainScreenFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_main_screen, container, false);
+    }
+}

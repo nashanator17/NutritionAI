d331755624b952d97985457632b3aeccf665bca3
danieljabbour <jabbourd@mcmaster.ca>
2018-07-18 11:04:14 -0400

    Added chart capability, Still resolving fragment embedding problems

--- MobileDev/app/src/main/java/com/example/djabbour/basquette/MainScreenActivity.java
+++ MobileDev/app/src/main/java/com/example/djabbour/basquette/MainScreenActivity.java
@@ -1,11 +1,20 @@
 package com.example.djabbour.basquette;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+
+import java.util.ArrayList;
+
+import static android.content.ContentValues.TAG;
 
 
 public class MainScreenActivity extends AppCompatActivity {
@@ -28,6 +37,51 @@ public class MainScreenActivity extends AppCompatActivity {
 
         //Logs details about activity it is working in
         Log.d(TAG, "onCreate: Starting to create chart");
+        pieChart = (PieChart) findViewById(R.id.pieChart);
+
+
+//        pieChart.setDescription("Nutrient percentages");
+        pieChart.setRotationEnabled(true);
+        pieChart.setHoleRadius(30f);
+        addDataSet();
+
+    }
+
+    private void addDataSet() {
+        Log.d(TAG, "addDataSet has begun");
+        ArrayList<PieEntry> yEntries = new ArrayList<>();
+        ArrayList<String> xEntries = new ArrayList<>();
+
+        for(int i=0;i<yData.length;i++){
+            yEntries.add(new PieEntry(yData[i], i));
+        }
+
+        for(int i=1;i<xData.length;i++){
+            xEntries.add(xData[i]);
+        }
+
+        //Create data set
+        PieDataSet pieDataSet = new PieDataSet(yEntries, "Nutrients");
+        pieDataSet.setSliceSpace(1);
+        pieDataSet.setValueTextSize(8);
+
+        ArrayList<Integer> colors = new ArrayList<>();
+        colors.add(Color.BLUE);
+        colors.add(Color.RED);
+        colors.add(Color.GREEN);
+
+        pieDataSet.setColors(colors);
+
+        //Add legend to chart
+
+        Legend legend = pieChart.getLegend();
+        legend.setForm(Legend.LegendForm.CIRCLE);
+        legend.setPosition(Legend.LegendPosition.LEFT_OF_CHART); //depracated, fix this
+
+        //Create pie data object
+        PieData pieData = new PieData(pieDataSet);
+        pieChart.setData(pieData);
+        pieChart.invalidate();
 
     }
 
--- MobileDev/app/src/main/java/com/example/djabbour/basquette/MainScreenFragment.java
+++ MobileDev/app/src/main/java/com/example/djabbour/basquette/MainScreenFragment.java
@@ -2,17 +2,14 @@ package com.example.djabbour.basquette;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import static android.content.ContentValues.TAG;
+
 public class MainScreenFragment extends Fragment {
-
-
-    public static MainScreenFragment newInstance() {
-        MainScreenFragment fragment = new MainScreenFragment();
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,6 +18,7 @@ public class MainScreenFragment extends Fragment {
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Log.d(TAG, "OK OK OK ");
         return inflater.inflate(R.layout.activity_main_screen, container, false);
     }
 }
